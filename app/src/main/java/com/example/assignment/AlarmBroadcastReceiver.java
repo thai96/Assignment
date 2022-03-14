d09c658bcd4096bfc5f5f5ad0737a1f360c6b3fc
ChisKien <chiskien03214@gmail.com>
2022-03-14 14:42:51 +0700

    Add StartAlarmService in BroadcastReceiver

--- app/src/main/java/com/example/assignment/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/example/assignment/AlarmBroadcastReceiver.java
@@ -1,4 +1,85 @@
 package com.example.assignment;
 
-public class AlarmBroadcastReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.text.CaseMap;
+import android.os.Build;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class AlarmBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String MONDAY = "MONDAY";
+    public static final String TUESDAY = "TUESDAY";
+    public static final String WEDNESDAY = "WEDNESDAY";
+    public static final String THURSDAY = "THURSDAY";
+    public static final String FRIDAY = "FRIDAY";
+    public static final String SATURDAY = "SATURDAY";
+    public static final String SUNDAY = "SUNDAY";
+    public static final String RECURRING = "RECURRING";
+    public static final String TITLE = "TITLE";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) { //check when the application service is boot up
+            String toastText = "Alarm Reboot"; // khoi tao 1 toast text thong bao alarm da reboot
+            Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
+            startRescheduleAlarmsService(context); //neu nhu
+        } else {
+            String toastText = "Alarm Received";
+            Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+            if (!intent.getBooleanExtra(RECURRING, false)) {
+                startAlarmService(context, intent);
+            }
+            {
+                if (alarmIsToday(intent)) {
+                    startAlarmService(context, intent);
+                }
+            }
+        }
+    }
+
+    private boolean alarmIsToday(Intent intent) {
+        Calendar calendar = Calendar.getInstance(); //get instance of calendar
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        int today = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (today) {
+            case Calendar.MONDAY:
+                return intent.getBooleanExtra(MONDAY, false);
+            case Calendar.TUESDAY:
+                return intent.getBooleanExtra(TUESDAY, false);
+            case Calendar.WEDNESDAY:
+                return intent.getBooleanExtra(WEDNESDAY, false);
+            case Calendar.THURSDAY:
+                return intent.getBooleanExtra(THURSDAY, false);
+            case Calendar.FRIDAY:
+                return intent.getBooleanExtra(FRIDAY, false);
+            case Calendar.SATURDAY:
+                return intent.getBooleanExtra(SATURDAY, false);
+            case Calendar.SUNDAY:
+                return intent.getBooleanExtra(SUNDAY, false);
+        }
+        return false;
+    }
+
+    private void startAlarmService(Context context, Intent intent) {
+        Intent intentService = new Intent(context,AlarmService.class);
+        intentService.putExtra(TITLE,intent.getStringExtra(TITLE));
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intentService);
+        } else {
+            context.startService(intentService);
+        }
+    }
+
+    private void startRescheduleAlarmsService(Context context) {
+        Intent intentService = new Intent(context, RescheduleAlarmsService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intentService);
+        } else {
+            context.startService(intentService);
+        }
+    }
 }
