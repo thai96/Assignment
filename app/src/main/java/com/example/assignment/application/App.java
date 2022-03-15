d2b9139c1b5467e9b26d3d979e14a4ca570c044d
Phạm Quang Thái <73540939+thai96@users.noreply.github.com>
2022-03-15 22:37:20 +0700

    fix bug in notification

--- app/src/main/java/com/example/assignment/application/App.java
+++ app/src/main/java/com/example/assignment/application/App.java
@@ -4,17 +4,11 @@ import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Build;
-
-import androidx.annotation.RequiresApi;
 
 public class App extends Application {
     //ck
-    public static final String CHANNEL_ID = "com.example.assignment.application";
-    public static final String CHANNEL_NAME = "ALARM_SERVICE_CHANNEL_NAME";
-
+    public static final String CHANNEL_ID = "ALARM_SERVICE_CHANNEL";
 
     @Override
     public void onCreate() {
@@ -23,19 +17,13 @@ public class App extends Application {
     }
 
     private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = CHANNEL_NAME;
-            String description = "Alarm Service";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            if(notificationManager != null)
-            notificationManager.createNotificationChannel(channel);
-        }
+        NotificationChannel serviceChannel = new NotificationChannel(
+                CHANNEL_ID,
+                "Alarm Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(serviceChannel);
+
     }
 }
--- app/src/main/java/com/example/assignment/service/AlarmService.java
+++ app/src/main/java/com/example/assignment/service/AlarmService.java
@@ -4,22 +4,16 @@ import static com.example.assignment.AlarmBroadcastReceiver.TITLE;
 import static com.example.assignment.application.App.CHANNEL_ID;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 
-import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import com.example.assignment.R;
 import com.example.assignment.RingActivity;
@@ -52,26 +46,16 @@ public class AlarmService extends Service {
                 notificationIntent, 0);
         String alarmTitle = String.format("%s Alarm", intent.getStringExtra(TITLE));
 
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-//                .setContentTitle(alarmTitle)
-//                .setContentText("Wake up motherfucker")
-//                .setSmallIcon(R.drawable.baseline_alarm_24)
-//                .setContentIntent(pendingIntent);
-        Notification notification =
-                new NotificationCompat.Builder(this, CHANNEL_ID)
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle(alarmTitle)
                 .setContentText("Wake up motherfucker")
                 .setSmallIcon(R.drawable.baseline_alarm_24)
-                        .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setContentIntent(pendingIntent).build();
+                .setContentIntent(pendingIntent)
+                .build();
 
         mediaPlayer.start();
         long[] pattern = {0, 100, 1000};
         vibrator.vibrate(VibrationEffect.createWaveform(pattern, 0));
-//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-//
-//// notificationId is a unique int for each notification that you must define
-//        notificationManager.notify(1, builder.build());
         startForeground(1, notification);
         return START_STICKY;
     }
@@ -82,5 +66,4 @@ public class AlarmService extends Service {
         mediaPlayer.stop();
         vibrator.cancel();
     }
-
 }
